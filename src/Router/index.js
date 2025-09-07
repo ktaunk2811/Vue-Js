6ac99170e4e62a6b89f615e7e4a01c7d344267c9
Khush Taunk <khush.taunk@maersk.com>
2025-09-07 15:57:49 +0530

    added json-server

--- src/Router/index.js
+++ src/Router/index.js
@@ -2,24 +2,30 @@ import { createRouter,createWebHistory } from "vue-router";
 import Homeview from "../views/Homeview.vue";
 import JobsView from "../views/JobsView.vue";
 import NotFoundView from "../views/NotFoundView.vue";
+import JobView from "../views/JobView.vue";
 const router=createRouter({
     history:createWebHistory(import.meta.env.BASE_URL),
     routes:[
         {
         path: '/',
         name: 'home',
-        component :Homeview
+        component :Homeview,
         },
         {
         path: '/jobs',
         name: 'jobs',
-        component :JobsView
+        component :JobsView,
         },
         {
-        path: '/jobs/add',
-        name: 'Add jobs',
-        component :NotFoundView
-        }
+        path:'/jobs/:id',
+        name: 'job',
+        component :JobView,
+        },
+        {
+        path:'/:catchAll(.*)',
+        name: 'not-found',
+        component :NotFoundView,
+        },
     ]
 })
 
