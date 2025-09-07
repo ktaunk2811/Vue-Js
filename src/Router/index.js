cf620def7fa50819b4e5f5ca8f10f37febe21832
Khush Taunk <khush.taunk@maersk.com>
2025-09-07 15:45:06 +0530

    added the notfound view , addjobs currently pointing to it

--- src/Router/index.js
+++ src/Router/index.js
@@ -1,6 +1,7 @@
 import { createRouter,createWebHistory } from "vue-router";
 import Homeview from "../views/Homeview.vue";
 import JobsView from "../views/JobsView.vue";
+import NotFoundView from "../views/NotFoundView.vue";
 const router=createRouter({
     history:createWebHistory(import.meta.env.BASE_URL),
     routes:[
@@ -13,6 +14,11 @@ const router=createRouter({
         path: '/jobs',
         name: 'jobs',
         component :JobsView
+        },
+        {
+        path: '/jobs/add',
+        name: 'Add jobs',
+        component :NotFoundView
         }
     ]
 })
